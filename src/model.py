8985dc8eab87520c90484b08a910eb92e25828ea
Elaina <1463967532@qq.com>
2025-11-11 15:44:05 +0000

    增加推理.mlp模型可能过于简单

--- src/model.py
+++ src/model.py
@@ -10,7 +10,7 @@ class MLPModel(nn.Module):
         self.linear2 = nn.Linear(hidden_size, output_size)
         self.device=device
         self.to(device)  # 关键：初始化时就把模型移到目标设备
-    def forward(self, x:torch.Tensor):
+    def forward(self, x:torch.Tensor)->torch.Tensor:
         assert isinstance(x, torch.Tensor),  "输入必须是torch.Tensor类型"
         x = x.to(self.device)  # 确保输入数据在正确的设备上
         x = self.linear1(x)
