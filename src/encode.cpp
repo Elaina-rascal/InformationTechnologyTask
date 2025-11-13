1648a31710c63633b04f8eb8464ae831a92036e3
Elaina <1463967532@qq.com>
2025-11-13 10:28:22 +0000

    增加读取第一行数据

--- src/encode.cpp
+++ src/encode.cpp
@@ -1,3 +1,16 @@
-int main() {
+#include "rapidcsv.h"
+#include <string>
+#include <vector>
+int main()
+{
+    auto csv_path = "/pytorch/data/task1.csv";
+    // io::CSVReader<0> in("data_44cols.csv");
+    rapidcsv::Document doc(csv_path);
+
+    std::vector<std::string> row = doc.GetRow<std::string>(0);
+    for (size_t i = 0; i < row.size(); ++i)
+    {
+        printf("第 %d 行: %s\n", static_cast<int>(i + 1), row[i].c_str());
+    }
     return 0;
 }
--- src/include/read_from_excel.hpp
+++
@@ -1,5 +0,0 @@
-#ifndef __READ_FROM_EXCEL_HPP__
-#define __READ_FROM_EXCEL_HPP__
-#include "csv.h"
-
-#endif
